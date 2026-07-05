266dfec0bc3749be60b5a84acec7646031240af8
Mr fa <59249447+jcyh6923@users.noreply.github.com>
2026-07-05 19:39:34 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -31,7 +31,6 @@ const Footer = () => {
         id='footer-bottom'
         className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
         <div id='footer-bottom-left' className='text-center lg:text-start'>
-          <PoweredBy />
           <div className='flex gap-x-1'>
             <CopyRightDate />
             <a
@@ -39,7 +38,6 @@ const Footer = () => {
               className='underline font-semibold dark:text-gray-300 '>
               {siteConfig('AUTHOR')}
             </a>
-            {BIO && <span className='mx-1'> | {BIO}</span>}
           </div>
         </div>
 
