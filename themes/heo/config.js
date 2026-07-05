ae960a537329d83210ebc5c0eadb9a17f2450987
Mr fa <59249447+jcyh6923@users.noreply.github.com>
2026-07-05 19:02:27 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: '没事',
+  HEO_HERO_TITLE_2: '瞎折腾',
   HEO_HERO_TITLE_3: 'blog.coolc.de',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
